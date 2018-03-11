05d51d0148c5555111f60e2bb6039600d20fa7a6
ErikBoesen <me@erikboesen.com>
2018-03-10 20:52:39 -0500

    Remove Chrome restrictions as root

--- root.sh
+++ root.sh
@@ -8,6 +8,8 @@ rm -rf /Applications/Adobe*
 # TODO: Remove Adobe application support files etc.
 echo "Disabling iTunes..."
 rm -rf /Applications/iTunes.app/Contents/Info.plist
-
+echo "Disabling inconvenient Chrome restrictions..."
+plutil -remove DeveloperToolsDisabled /Library/Managed\ Preferences/com.google.Chrome.plist
+plutil -remove IncognitoModeAvailability /Library/Managed\ Preferences/com.google.Chrome.plist
 echo "Resetting root password..."
 passwd
