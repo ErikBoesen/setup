d379cfa16cba721cedcf265acffcf4911df75809
ErikBoesen <me@erikboesen.com>
2018-06-12 14:10:25 -0400

    Add task function

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ start=$(date +%s)
 
 app_target="$HOME/Applications"
 
+function task {
+    echo "$1..."
+}
+
 function install_dmg {
     # Parameters:
     #   install_dmg 1(App name) 2(Download URL) 3(DMG filename)
@@ -13,7 +17,7 @@ function install_dmg {
         echo "$1 is already installed."
         return
     fi
-    echo "Installing $1..."
+    task "Installing $1"
     curl -o /tmp/$3 $2
     hdiutil mount /tmp/$3
     cp -r /Volumes/$1/*.app $app_target/
@@ -29,7 +33,7 @@ function install_zip {
         echo "$1 is already installed."
         return
     fi
-    echo "Installing $1..."
+    task "Installing $1"
     curl -Lo /tmp/$3 $2
     unzip -q /tmp/$3 -d /tmp/
     mv /tmp/$1.app $app_target/
@@ -54,21 +58,21 @@ if ! brew --version >/dev/null; then
     exit 1
 fi
 
-echo "Installing & updating Homebrew packages (bg)..."
+task "Installing & updating Homebrew packages (bg)"
 (cat res/packages_brew.txt | xargs brew install && brew update) >/dev/null &
 
-echo "Installing crontab..."
+task "Installing crontab"
 crontab res/crontab
 
 open "https://github.com/login"
 read -p "Please sign into GitHub before running. Press enter to continue." _
 
-echo "Cloning bin..."
+task "Cloning bin"
 git clone -q https://github.com/ErikBoesen/macbin ~/.bin &
-(echo "Cloning dotfiles..."
+(task "Cloning dotfiles"
 git clone -q https://github.com/ErikBoesen/.files ~/.files
 
-echo "Bootstrapping dotfiles..."
+task "Bootstrapping dotfiles"
 ~/.files/bootstrap.sh) &
 
 install_dmg "Keybase" "https://prerelease.keybase.io/Keybase.dmg" "Keybase.dmg"
@@ -89,30 +93,30 @@ curl -Lks "https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.05
 unzip "/tmp/1.050R-it.zip"
 open "/tmp/source-code-pro-2.030R-ro-1.050R-it/OTF/*") &
 
-echo "Installing oh-my-zsh..."
+task "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Opening Unsplash page with a search for 'parrot' so you can find a desktop background."
 open "https://unsplash.com/search/parrot"
 
-echo "Disabling space rearranging based on recent use..."
+task "Disabling space rearranging based on recent use"
 defaults write com.apple.dock mru-spaces -bool false
 
-echo "Disable opening Photos on plug..."
+task "Disable opening Photos on plug"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
-echo "Changing graphics settings for speed..."
+task "Changing graphics settings for speed"
 # https://www.defaults-write.com/10-terminal-commands-to-speed-up-macos-sierra-on-your-mac/
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 defaults write -g QLPanelAnimationDuration -float 0
 defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-echo "Disabling mission control and spaces..."
+task "Disabling mission control and spaces"
 # https://www.defaults-write.com/mac-os-x-disable-mission-control-and-spaces/
 defaults write com.apple.dock mcx-expose-disabled -bool TRUE
 
-echo "Disabling Dashboard..."
+task "Disabling Dashboard"
 defaults write com.apple.dashboard mcx-disabled -bool TRUE
 
 echo "Removing Launchpad animations.."
@@ -121,38 +125,38 @@ echo "Removing Launchpad animations.."
 defaults write com.apple.dock springboard-show-duration -int 0
 defaults write com.apple.dock springboard-hide-duration -int 0
 
-echo "Showing full POSIX path in Finder header..."
+task "Showing full POSIX path in Finder header"
 # https://www.defaults-write.com/display-full-posix-path-in-os-x-finder-title-bar/
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-echo "Disabling two-finger swipe in Chrome..."
+task "Disabling two-finger swipe in Chrome"
 # https://www.defaults-write.com/disable-the-two-finger-swipe-gesture-in-chrome/
 defaults write com.google.Chrome.plist AppleEnableSwipeNavigateWithScrolls -bool FALSE
 
-echo "Disabling .DS_Store files..."
+task "Disabling .DS_Store files"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
-echo "Installing erkbsn zsh theme..."
+task "Installing erkbsn zsh theme"
 curl -Lo $HOME/.oh-my-zsh/themes/erkbsn.zsh-theme "https://raw.github.com/ErikBoesen/erkbsn/master/erkbsn.zsh-theme"
 
 echo "Done with configuration! Beginning independent installs."
 
-echo "Making unlocked System Preferences app..."
+task "Making unlocked System Preferences app"
 cp -r /Applications/System\ Preferences.app $app_target
 rm $app_target/System\ Preferences.app/Contents/Resources/NSPrefPaneGroups.xml
 
-echo "Installing GIMP with Homebrew..."
+task "Installing GIMP with Homebrew"
 brew cask install gimp --appdir=$app_target
-#echo "Opening GIMP download page..."
+#task "Opening GIMP download page"
 ## TODO: Auto-download
 #open "https://www.gimp.org/downloads/"
 
-echo "Making \$GOPATH..."
+task "Making \$GOPATH"
 mkdir -p /usr/local/go
 
 install_zip "Atom" "https://atom.io/download/mac" "atom-mac.zip"
 
-echo "Installing Atom packages (bg)..."
+task "Installing Atom packages (bg)"
 while read package; do
     if [[ ! -d "$HOME/.atom/packages/$package" ]]; then
         apm install $package &
@@ -162,20 +166,20 @@ done < res/packages_apm.txt
 install_dmg "Google Chrome" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" "googlechrome.dmg"
 install_zip "Install Spotify" "https://download.scdn.co/SpotifyInstaller.zip" "SpotifyInstaller.zip"
 
-echo "Recreating source directory from backup..."
-echo "Copying from server..."
+task "Recreating source directory from backup"
+task "Copying from server"
 scp juno:"dump-*/{src.tar,repos.txt}" /tmp/ &&
-echo "Extracting..." &&
+task "Extracting" &&
 tar -xkf /tmp/src.tar -C ~/src
 
 while read repo; do
     git clone "$repo" ~/src/$repo
 done < /tmp/repos.txt
 
-echo "Showing hidden files..."
+task "Showing hidden files"
 defaults write com.apple.finder AppleShowAllFiles YES
 
-echo "Showing all file extensions..."
+task "Showing all file extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 wait
